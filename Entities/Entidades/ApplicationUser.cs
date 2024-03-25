c2d81ef0886a13e44879461d27721dc058c4213f
Nelson Cassemiro <nelsoncassemiro@gmail.com>
2024-03-25 15:29:52 -0300

    Criando Entidades relacionadas

---
+++ Entities/Entidades/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entidades
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Column("USR_CPF")]
+        public String CPF { get; set; }
+    }
+}
